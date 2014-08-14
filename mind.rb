474a0ce8e309e2dca4f9ae47355fc42ebb780b5e
faizaanshamsi <faizaan2000@gmail.com>
2014-08-14 10:47:00 -0400

    only create 1 server

--- mind.rb
+++ mind.rb
@@ -6,13 +6,15 @@ require "socket"
 connection :neurosky, adaptor: :neurosky, port: '/dev/tty.MindWaveMobile-DevA'
 device :neurosky, driver: :neurosky, interval: 0.1
 
+
 def handle_eeg(sender, data)
-  dts = TCPServer.new('localhost', 20000)
-  Thread.start(dts.accept) do |s|
+  @dts ||= TCPServer.new('127.0.0.1', 20000)
+  Thread.start(@dts.accept) do |s|
     print(s, " is accepted\n")
     s.write(data)
     print(s, " is gone\n")
     s.close
+    puts data
   end
 end
 
